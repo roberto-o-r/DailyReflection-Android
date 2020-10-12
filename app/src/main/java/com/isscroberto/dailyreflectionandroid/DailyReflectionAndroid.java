856daaf305114925a22d5dbff6cf0f94a9ce4d9d
Roberto Orozco <issc.roberto@outlook.com>
2020-10-12 18:13:55 -0500

    Update crashlytics

--- app/src/main/java/com/isscroberto/dailyreflectionandroid/DailyReflectionAndroid.java
+++ app/src/main/java/com/isscroberto/dailyreflectionandroid/DailyReflectionAndroid.java
@@ -26,5 +26,8 @@ public class DailyReflectionAndroid extends Application {
                 .setCriticalFeedbackCollectors(new DefaultEmailFeedbackCollector(getString(R.string.my_email)))
                 .applyAllDefaultRules();
         //.setAlwaysShow(BuildConfig.DEBUG);
+
+        // Force a crash
+        //throw new RuntimeException("Test Crash");
     }
 }
